0e6463521e9038d75b6d9b719572cb701195c43b
hamidreza.shoghi <hamidreza.shoghi@snapp.cab>
2024-02-28 18:26:00 +0330

    Using dbus to restart jaeger collector unitd

--- main.go
+++ main.go
@@ -3,12 +3,14 @@ package main
 import (
 	"context"
 	"fmt"
-	"github.com/coreos/go-systemd/v22/dbus"
 	"log"
 	"net"
 	"os"
 	"os/exec"
 	"strings"
+	"time"
+
+	"github.com/coreos/go-systemd/v22/dbus"
 )
 
 func main() {
@@ -25,33 +27,18 @@ func main() {
 
 	// Get the unit name of the service you want to restart
 	unitName := "jaeger-collector.service"
-
-	// Get the unit properties
-	props, err := conn.GetAllPropertiesContext(ctx, unitName)
+	
+	mode := "replace"
+	_, err = conn.RestartUnitContext(ctx, unitName, mode, nil)
 	if err != nil {
-		log.Fatalf("Failed to get unit properties: %v", err)
-	}
-
-	activeState, ok := props["ActiveState"].(string)
-	if !ok {
-		log.Fatalf("Failed to get active state for unit: %s", unitName)
-	}
-
-	if activeState == "active" {
-		// Restart the service
-		cmd := exec.Command("systemctl", "restart", unitName)
-		cmd.Stdout = os.Stdout
-		cmd.Stderr = os.Stderr
-		err := cmd.Run()
-		if err != nil {
-			log.Fatalf("Failed to restart service: %v", err)
-		}
-		fmt.Printf("Service '%s' restarted\n", unitName)
+		log.Fatalf("Failed to restart service: %v", err)
 	} else {
-		fmt.Printf("Service '%s' is not currently active\n", unitName)
+		fmt.Printf("Service '%s' restarted\n", unitName)
 	}
 
-	// Get bridge interface name and its IP
+	time.Sleep(3 * time.Second)
+	
+	// Get bridge interface and its IP and range
 	var bridgeInterfaceName string
 	var bridgeAddress string
 	var bridgeAddress16BitBlock net.IP
@@ -63,12 +50,12 @@ func main() {
 	for _, i := range ifaces {
 		addrs, err := i.Addrs()
 		if err != nil {
-			log.Fatal("Couldn't get interface addresses, %v\n", err.Error())
+			log.Fatalf("Couldn't get interface addresses, %v\n", err.Error())
 		}
 		for _, a := range addrs {
 			switch v := a.(type) {
 			case *net.IPNet:
-				if strings.HasPrefix(i.Name, "enp") && v.IP.To4() != nil {
+				if strings.HasPrefix(i.Name, "br-") && v.IP.To4() != nil {
 					bridgeInterfaceName = i.Name
 					bridgeAddress = strings.Split(v.IP.To4().String(), "/")[0] // 192.168.65.5
 					bridgeAddress = strings.TrimSpace(bridgeAddress)
@@ -78,7 +65,6 @@ func main() {
 		}
 	}
 
-	// Get bridge ip address as 16-bit Block
 	ipv4mask := net.CIDRMask(16, 32)
 	bridgeAddress16BitBlock = bridgeAddress16BitBlock.Mask(ipv4mask)
 	rangeIn16BitBlock := bridgeAddress16BitBlock.String() + "/16"
